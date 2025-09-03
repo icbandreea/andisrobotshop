43478c583924ca896d6da3ffc24b2e76039dce1d
icbandreea <icb.andreea@yahoo.com>
2025-09-03 14:49:40 +0300

    added routing, defined and accessed query string parameters and styled active links

---
+++ src/app/app-routing-module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent, title: "Home - Andi's Robot Shop"},
+  {path: 'catalog', component: CatalogComponent, title: "Catalog - Andi's Robot Shop"},
+  {path: 'cart', component: CartComponent, title: "Cart- Andi's Robot Shop"},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IProduct } from './product.model';
-import { CartService } from '../cart.service';
+import { ProductService } from './product.service';
+import { CartService } from '../cart/cart.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-catalog',
@@ -12,52 +14,25 @@ export class CatalogComponent {
   filter: string ="";
  
 
-  constructor(private cartSvc: CartService) {
-    this.products = [
-      {
-    id: 1,
-    description:
-      "A robot head with an unusually large eye and teloscpic neck -- excellent for exploring high spaces.",
-    name: "Large Cyclops",
-    imageName: "head-big-eye.png",
-    category: "Heads",
-    price: 1220.5,
-    discount: 0.2,
-  },
-  {
-    id: 17,
-    description: "A spring base - great for reaching high places.",
-    name: "Spring Base",
-    imageName: "base-spring.png",
-    category: "Bases",
-    price: 1190.5,
-    discount: 0,
-  },
-  {
-    id: 6,
-    description:
-      "An articulated arm with a claw -- great for reaching around corners or working in tight spaces.",
-    name: "Articulated Arm",
-    imageName: "arm-articulated-claw.png",
-    category: "Arms",
-    price: 275,
-    discount: 0,
-  },
-  {
-    id: 2,
-    description:
-      "A friendly robot head with two eyes and a smile -- great for domestic use.",
-    name: "Friendly Bot",
-    imageName: "head-friendly.png",
-    category: "Heads",
-    price: 945.0,
-    discount: 0.2,
-  },
-    ]
+  constructor(
+    private cartSvc: CartService, 
+    private productSvc: ProductService, 
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.productSvc.getProducts().subscribe(products => {
+      this.products = products;
+    });
+    this.route.queryParams.subscribe((params) => {
+      this.filter = params['filter'] ?? '';
+    });
   }
 
   addToCart(product: IProduct) {
     this.cartSvc.add(product);
+    this.router.navigate(['/cart']);
   }
 
 
