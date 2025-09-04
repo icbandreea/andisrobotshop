6fda0f59e56eda9446da56830068e778f05e7c86
icbandreea <icb.andreea@yahoo.com>
2025-09-04 10:38:19 +0300

    creating template-driven form, using template variables, working with multiple validators

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent, title: "Home - Andi's Robot Shop"},
   {path: 'catalog', component: CatalogComponent, title: "Catalog - Andi's Robot Shop"},
   {path: 'cart', component: CartComponent, title: "Cart- Andi's Robot Shop"},
+  {path: 'sign-in', component: SignInComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
